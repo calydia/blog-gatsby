9fcec5590fd85e2a92705e413819a111ae067314
Sanna Mäkinen <sanna.makinen@gmail.com>
2021-04-04 14:13:55 +0300

    Chore: Change the favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,31 @@ module.exports = {
       options: {
         icons: [
           {
+            "src": "/icons/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/icons/android-chrome-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "/icons/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "/icons/icon-16x16.png",
+            "sizes": "48x48",
+            "type": "image/png"
+          },
+          {
+            "src": "/icons/icon-32x32.png",
+            "sizes": "48x48",
+            "type": "image/png"
+          },
+          {
             "src": "/icons/icon-48x48.png",
             "sizes": "48x48",
             "type": "image/png"
